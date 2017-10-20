2306ebc76c53713ab076423b8ae834a7639fce90
pesh12 <ekocibar@hotmail.com>
2017-10-20 02:18:42 +0200

    scripting part has finished

--- index.js
+++ index.js
@@ -3,35 +3,80 @@
 
 function GitHubUser (username) {
     // Fill this
+    this.username = username;
 }
 
-GitHubUser.prototype.getUserInformation = function () {
+GitHubUser.prototype.getUserInformations = function () {
     // Fill this
+    let url = "https://api.github.com/users/" + this.username;
+	return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        if (window.XMLHttpRequest) {
+            request = new XMLHttpRequest();
+        } else {
+            request = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        request.open("GET", url);
+        request.onreadystatechange = () => {
+            if ((request.readyState===4) && (request.status===200)) {
+                resolve(request.response);
+            }
+        };
+        request.send();
+    });
 };
 
 GitHubUser.prototype.getRepos = function () {
     // Fill this
+    let url = "https://api.github.com/users/" + this.username + "/repos";
+	return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        if (window.XMLHttpRequest) {
+            request = new XMLHttpRequest();
+        } else {
+            request = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        request.open("GET", url);
+        request.onreadystatechange = () => {
+            if ((request.readyState===4) && (request.status===200)) {
+                resolve(request.response);
+            }
+        };
+        request.send();
+  });
 }
 
 GitHubUser.prototype.render = function () {
     // Here return a string, be fancy and use map && reduce
+    let user = JSON.parse(this.user);
+    //console.log(user);
+	let display = "<h3>User</h3>";
+	display += "<div id='user'><p>Name : " + user.name + "<br>Created at : " + user.created_at + "<br>Last Login :" + user.updated_at + "<br> <a target='_blank' href=" + user.html_url + ">Go to GitHub Page : </a><br><img id='image' src='" + user.avatar_url + "' /></p>  </div>";
+	
+	let repo = JSON.parse(this.repos);
+    console.log(repo);
+    let repoItems = repo.reduce((a, b) =>  a + '<li><a target="_blank" href=' + b.html_url + '>' + b.name + '</a></li>', '');
+	display += "<h3>Repositories</h3>";
+	display += '<ul id = "repoList">' + repoItems +	'</ul>';
+	return display;
 }
 
 function Render($element, html){
     // Fill this
+    document.querySelector($element).innerHTML = html;
 }
 
 
 // Expecting
-let gitUser = new GitHubUser('ideabile');
+let gitUser = new GitHubUser('mkruijt');
 
 gitUser
-    .getUserInformations()
+    .getUserInformations()  
     .then(function(informations){
-        this.user = informations;
-        return this.getRepos();
-    })
+        gitUser.user = informations;
+        return gitUser.getRepos();
+    }) 
     .then(function(repos) {
-        this.repos = repos;
-        Render('.githubView', this.render());
+        gitUser.repos = repos;
+        Render('.githubView', gitUser.render());
     });
